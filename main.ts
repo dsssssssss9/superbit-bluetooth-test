98c4811374e60342d3350e66f7448774c540c5e4
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2025-04-28 16:39:37 -0500

    added wiring colour codes / changed to non personal photo

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+/**
+ * DC motor connected to M!
+ */
 bluetooth.onBluetoothConnected(function () {
     basic.showIcon(IconNames.Yes)
 })
@@ -10,13 +13,24 @@ basic.showIcon(IconNames.Duck)
 lcdDisplay.lcdInitIIC()
 lcdDisplay.lcdClearAll()
 bluetooth.startUartService()
+/**
+ * LCD Screen Wiring Colour Code
+ * 
+ * Red     ----->     3V
+ * 
+ * Black   ----->     GND
+ * 
+ * Green   ----->     SCL
+ * 
+ * Blue    ----->     SDA
+ */
 basic.forever(function () {
     SerialData = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
     if (SerialData == "A") {
         lcdDisplay.lcdSetBgIamge("fruit.png")
         SuperBit.MotorRun(SuperBit.enMotors.M1, 138)
     } else if (SerialData == "B") {
-        lcdDisplay.lcdSetBgIamge("cooper1.jpg")
+        lcdDisplay.lcdSetBgIamge("building.png")
         SuperBit.MotorStopAll()
     }
 })
